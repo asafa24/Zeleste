abc1310a7ba528e7f6b3642ebfff6d40e3e661e4
asafa24 <saidiasafa1@gmail.com>
2026-04-05 20:39:43 +0200

    Implementation of vector and containers

--- zeleste/src/main/java/com/iza/zeleste/Main.java
+++ zeleste/src/main/java/com/iza/zeleste/Main.java
@@ -3,19 +3,38 @@ package com.iza.zeleste;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("ui.fxml"));
+        AnchorPane uiPane = loader.load();
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(canvas, uiPane);
+
+
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("Zeleste");
         stage.setScene(scene);
         stage.show();
     }
+    public static void main(String[] args) {
+        launch();
+    }
+
+
 
 
 }
---
+++ zeleste/src/main/java/com/iza/zeleste/Vector2D.java
@@ -0,0 +1,32 @@
+package com.iza.zeleste;
+
+public class Vector2D {
+    public double x, y;
+
+    public Vector2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void add(Vector2D v){
+        this.x += v.x;
+        this.y += v.y;
+    }
+
+    public void multiply(double scal){
+        this.x *= scal;
+        this.y *= scal;
+    }
+
+    public double magnitude(){
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+
+    public void normalize(){
+        double mag = this.magnitude();
+        if(mag != 0){
+            this.x /= mag;
+            this.y /= mag;
+        }
+    }
+}
